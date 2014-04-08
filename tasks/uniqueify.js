a11fe8161fecbd83f1bcaa8eae0f4cffc4b3cfcb
AJ Michels <ajmichels.web@gmail.com>
2014-04-08 11:55:03 -0500

    Implementing actual project code.

--- tasks/uniqueify.js
+++ tasks/uniqueify.js
@@ -4,52 +4,82 @@
  *
  * Copyright (c) 2014 AJ Michels
  * Licensed under the MIT license.
+ *
+ * Thanks to Sebastiaan Deckers (https://github.com/cbas/grunt-rev)
+ *
  */
 
 'use strict';
 
+var crypto = require('crypto');
+var path = require('path');
+var fs = require('fs');
+
 module.exports = function(grunt) {
 
-    // Please see the Grunt documentation for more information regarding task
-    // creation: http://gruntjs.com/creating-tasks
+    var fileHash = function(filepath, algorithm) {
+        var hash = crypto.createHash(algorithm);
+        grunt.log.verbose.write('Hashing ' + filepath + '...');
+        hash.update(grunt.file.read(filepath));
+        return hash.digest('hex');
+    };
 
-    grunt.registerMultiTask('static_version', 'A plugin for creating unique names for static content. (js, css, img files)', function() {
+    var textReplace = function(src, find, replace, filePath) {
+        var newText = src.replace(find, replace);
+        if (newText != src) {
+            grunt.log.write(filePath + ' : ' + find + ' ').ok(replace);
+        }
+        return newText;
+    };
 
-        // Merge task-specific and/or target-specific options with these defaults.
+    grunt.registerMultiTask('uniqueify', 'Updated file names and replace instances of the old name with the new name.', function(){
         var options = this.options({
-            punctuation: '.',
-            separator: ', '
+            algorithm: 'md5',
+            length: 8
         });
 
-        // Iterate over all specified file groups.
-        this.files.forEach(function(f) {
+        // Rename the file
+        grunt.event.on('uniqueify.rename', function(oldFile, newFile){
+            fs.renameSync(oldFile, newFile);
+            grunt.log.write(oldFile + ' ').ok(newFile);
+        });
 
-            // Concat specified files.
-            var src = f.src.filter(function(filepath) {
+        // Replace instances of filename in code
+        grunt.event.on('uniqueify.rename', function(oldFile, newFile){
+            if (!options.replaceSrc) {
+                grunt.log.warn('No src replacement paths (options.replaceSrc) defined in options.');
+            }
+            else {
+                var find = path.basename(oldFile);
+                var replace = path.basename(newFile);
+                var targets = grunt.file.expand(options.replaceSrc);
 
-                // Warn on and remove invalid source files (if nonull was set).
-                if (!grunt.file.exists(filepath)) {
-                    grunt.log.warn('Source file "' + filepath + '" not found.');
-                    return false;
-                } else {
-                    return true;
-                }
+                targets.forEach(function(filePath){
+                    grunt.file.copy(filePath, filePath, {
+                        process: function(text){
+                            return textReplace(text, find, replace, filePath);
+                        }
+                    });
+                });
+            }
 
-            }).map(function(filepath) {
+        });
 
-                // Read file source.
-                return grunt.file.read(filepath);
+        this.files.forEach(function(filePair){
+            var cwd = filePair.orig.cwd;
 
-            }).join(grunt.util.normalizelf(options.separator));
+            filePair.src.forEach(function(filePath) {
+                var root = path.resolve(path.dirname('.'));
+                var hash = fileHash(filePath, options.algorithm);
+                var prefix = hash.slice(0, options.length);
+                var renamed = [prefix, path.basename(filePath)].join('.');
+                var outPath = path.resolve(path.dirname(filePath), renamed);
 
-            // Handle options.
-            src += options.punctuation;
+                outPath = outPath.replace(root + '/', '');
 
-            // Write the destination file.
-            grunt.file.write(f.dest, src);
+                grunt.event.emit('uniqueify.rename', filePath, outPath);
 
-            // Print a success message.
-            grunt.log.writeln('File "' + f.dest + '" created.');
+            });
 
         });
 
